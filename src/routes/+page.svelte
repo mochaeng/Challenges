ff2a811c3c99e79b450077c7dad8c899f060e791
Luiz Campos <campos.antoonio@gmail.com>
2024-06-23 18:25:37 -0300

    fix: skeleton for deployment

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,38 @@
+<script lang="ts">
+	import wallpaper from '$lib/assets/wallpapers/chae.webp';
+
+	const challenges = [
+		{ name: 'Basics', path: '/begginers/basics' },
+		{ name: 'Error Page', path: '/begginers/error-page' }
+	];
+</script>
+
 <h1>Fronted Challenges</h1>
 
 <p class="">
-	A collection of some fronted challenges I solved on <a href="https://icodethis.com/">iCodeThis</a>
+	A collection of some fronted challenges I solved on <a
+		href="https://icodethis.com/"
+		target="_blank">iCodeThis</a
+	>
 </p>
 
 Begginers problem:
 
 <ul>
-	<li>
-		Basics
-		<a></a>
-	</li>
-	<li>
-		Basics
-		<a></a>
-	</li>
+	{#each challenges as challenge}
+		<li>
+			<a href={challenge['path']}>{challenge['name']}</a>
+		</li>
+	{/each}
 </ul>
 
-<style>
-	.container {
-		justify-content: center;
-		padding: 1em;
-	}
+<img
+	src={wallpaper}
+	alt="Chaeyoung from TWICE making a silence gesture to show she is the queen"
+	width="600px"
+/>
 
+<style>
 	ul {
 		list-style: none;
 	}
